bfe152ae069ca39856bcc1fa1eeb0df025b9bdc0
Harsh28-raj <harshraj4308g@gmail.com>
2025-08-26 22:47:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -25,33 +25,6 @@
                 navLinks.classList.remove('active');
             });
         });
-window.addEventListener('load', () => {
-            setTimeout(() => {
-                const loader = document.getElementById('loader');
-                loader.classList.add('loader-hidden');
-            }, 1000);
-        });
-
-        const hamburger = document.getElementById('hamburger');
-        const navLinks = document.getElementById('navLinks');
-
-        hamburger.addEventListener('click', () => {
-            navLinks.classList.toggle('active');
-        });
-
-      
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                const target = document.querySelector(this.getAttribute('href'));
-                target.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                });
-                navLinks.classList.remove('active');
-            });
-        });
 
    
-
         
